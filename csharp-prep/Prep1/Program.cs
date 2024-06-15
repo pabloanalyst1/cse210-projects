1f21e723d52ef3944ad077990d8c20796b6c9511
pabloanalyst1 <jparizandieta@gmail.com>
2024-06-15 17:27:57 -0600

    updated code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        //Ask the user for their name//
+
+        Console.WriteLine("What is your first name?");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your full name is {last}, {first} {last}");
+
     }
 }
